91b277f60f6566bd6a582c98f2c5fe0013ee8999
Ravin Samudika <raveensamudika@gmail.com>
2024-07-09 17:23:02 +0530

    Delete MemberQfit.Web directory

--- MemberQfit.Web/Models/ResponseDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MemberQfit.Web.Models
-{
-    public class ResponseDTO
-    {
-        public object? Result { get; set; }
-        public bool IsSuccess { get; set; } = true;
-        public string Message { get; set; } = "";
-    }
-}
